ccd0e6cc57ffe3ca77f7bf8f41a8327afdc68545
electronmods <80803369+electronmods@users.noreply.github.com>
2022-07-29 16:29:05 -0400

    Update gits_with_updates.sh

--- gits_with_updates.sh
+++ gits_with_updates.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 for e in `cat ~/working_list.txt` ; do
-( cd $e && git diff --name-status | \
+(
+    cd $e && git diff --name-status | \
     wc -l | \
-    ( read n && \
-    if [[ $n > 0 ]] ; then
-        echo $e
-    fi
+    (
+        read n && \
+        if [[ $n > 0 ]] ; then
+            echo $e
+        fi
     )
 )
 done
