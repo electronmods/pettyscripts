295d8f62b253ee364b1fb665458c7cf3a0f6a6ba
electronmods <80803369+electronmods@users.noreply.github.com>
2022-10-20 13:45:20 -0400

    Update python_cli_history.py

--- python_cli_history.py
+++ python_cli_history.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import readline; print('\n'.join([str(readline.get_history_item(i + 1)) for i in range(readline.get_current_history_length())]))
+# One-liner with a limit on most recent entries
+import readline; mostrecent=20; histsize=readline.get_current_history_length(); print('\n'.join([str(readline.get_history_item(i + 1)) for i in range(histsize-mostrecent, histsize)]))
 
+# Everything, and not one line
 import readline
 for i in range(readline.get_current_history_length()):
     print (readline.get_history_item(i + 1))
